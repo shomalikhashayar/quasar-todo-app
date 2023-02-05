f39e843c011156364397cab2da2cc7d11de38d67
Khashayar Shomali <khashi792017@gmail.com>
2023-02-05 17:25:28 +0330

    Create Add Task Area.
    Create Dialog Message for Deleting Tasks.

---
+++ quasar-todo-app-project/src/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">About</h5>
+    <p style="font-size: 1.2em">
+      This Todo Application is open source was created using VueJS and Quasar.
+      <br />
+      <br />
+      For Github Repo:
+      <a href="https://github.com/shomalikhashayar"> Click here!</a> <br />
+      Created by
+      <a
+        href="https://www.linkedin.com/in/khashayar-shomali-%D8%AE%D8%B4%D8%A7%DB%8C%D8%A7%D8%B1-%D8%B4%D9%85%D8%A7%D9%84%DB%8C-b445501b4/"
+        >Khashayar Shomali</a
+      >
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
